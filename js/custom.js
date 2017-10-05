b0379c3228b9772ab960ec0b2e8472fdb1d766c2
Silverman42 <snkeze9@gmail.com>
2017-10-05 19:03:09 +0100

    Footer has not been completed

---
+++ js/custom.js
@@ -0,0 +1,104 @@
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    jQuery.fn.extend({
+        navbarAnimate: function (offset, cssClass) {
+            return this.each(function () {  
+                if ($(window).scrollTop() >= offset) {
+                    $(this.children[0].children[0].children[1].children[0]).hide();
+                    $(this.children[0].children[0].children[1].children[1]).show();
+                } else {
+                    $(this.children[0].children[0].children[1].children[0]).show();
+                    $(this.children[0].children[0].children[1].children[1]).hide();
+                }
+            })
+        },
+        scrollAnimate: function (event, timing, animateArea, selectorHash) {
+            if (selectorHash !== "") {
+                event.preventDefault();
+                //hash content of the selected element
+                var hash = selectorHash;
+                //animating the scroll to move to the location of the hash
+                $(animateArea).animate({
+                    scrollTop: $(hash).offset().top
+                }, timing, function () {
+                    //Adding hash (#) to url after scrolling is done
+                    window.location.hash = hash;
+                })
+            }
+        },
+        cssAnimate: function (inAnimate) {
+            return this.each(function(){
+                if($(window).scrollTop() >= $(this).offset().top - 500){
+                    $(this).addClass(inAnimate);
+                }
+            });
+        },
+        cssAnimate2: function (trigger, offset, animateClass, retainAnimate) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            if (trigger == offset && retainAnimate == 'false') {
+                this.addClass('animated ' + animateClass)
+                    .one(animationEnd, function () {
+                        $(this).removeClass('animated ' + animateClass);
+                    })
+                return this;
+            } else if (trigger == offset && retainAnimate == 'true') {
+                this.velocity(animateClass, {
+                    loop: 2
+                });
+                return this;
+            }
+        }
+    });
+    var offset = []; //10;
+    var animateClass = [];
+    var trigger = [];
+    var retainState = [];
+    offset[0] = 10
+    animateClass[0] = "navbar-inverse-2";
+    $(".service-hide").cssAnimate('service-show')
+    $(".testimony-hide").cssAnimate('service-show');
+    $(".navbar").navbarAnimate(offset[0], animateClass[0]);
+    if($(window).scrollTop() > 10){
+        $('.logo').addClass('logo-reduce');
+    }
+    $(window).scroll(function () {
+        $(".navbar").navbarAnimate(offset[0], animateClass[0]);
+        $(".service-hide").cssAnimate('service-show')
+        $(".testimony-hide").cssAnimate('service-show');
+        if($(window).scrollTop() > 10){
+            $('.logo').addClass('logo-reduce');
+        }
+        else if($(window).scrollTop() < 30){
+            $('.logo').removeClass('logo-reduce');
+        }
+    })
+    var timing = 800;
+    var animateArea = "html, body";
+    var selectorHash;
+    $("#mapServices").click(function (event) {
+        selectorHash = this.hash;
+        $(this).scrollAnimate(event, timing, animateArea, selectorHash);
+    });
+    $("#mapHome").click(function (event) {
+        selectorHash = this.hash;
+        $(this).scrollAnimate(event, timing, animateArea, selectorHash);
+    });
+    $(".mapContact").click(function (event) {
+        selectorHash = this.hash;
+        $(this).scrollAnimate(event, timing, animateArea, selectorHash);
+    });
+    $("#mapAbout").click(function (event) {
+        selectorHash = this.hash;
+        $(this).scrollAnimate(event, timing, animateArea, selectorHash);
+    });
+    
+
+    /*-----Welcome Screen Animate---*/
+    //***$("#item-one").velocity({scale:1.3},{loop:true, duration: "slow"});
+});
+/*$(window).load(function(){
+    $("#item-one").velocity("stop")
+    .velocity({opacity:0});
+    $("#item-three").velocity({opacity:1},{duration:1000});
+    $(".welcome-screen").velocity({height:"0%",paddingTop:"0px"},{delay:2000});
+})*/
